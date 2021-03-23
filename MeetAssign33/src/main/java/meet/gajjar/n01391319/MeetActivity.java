9c5d2f84dfbb8258dd550cafbe00ef34abd792c5
MeetGajjar1319 <n01391319@humbermail.ca>
2021-03-23 16:35:54 -0400

    Initial Commit

---
+++ MeetAssign33/src/main/java/meet/gajjar/n01391319/MeetActivity.java
@@ -0,0 +1,14 @@
+package meet.gajjar.n01391319;
+//  Section C Meet Gajjar N01391319
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MeetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
